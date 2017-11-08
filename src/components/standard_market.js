612dccad33b936586f2a094ba88885c168da19f1
Luis Nesi <lenesi@hotmail.com>
2017-11-08 16:55:43 +0000

    Add Suspen to standard

--- src/components/standard_market.js
+++ src/components/standard_market.js
@@ -1,9 +1,10 @@
 import React , { Component } from 'react';
 import numeral from 'numeral';
+import { formatOutCome } from '../utils';
 export default class StandardMarket extends Component{
 	renderOutcome(outcome){
-		let price=numeral(outcome.price.decimal).format('0.00');
-		if(!this.props.use_decimal) price=outcome.price.num+"/"+outcome.price.den;
+		let price=formatOutCome(outcome,this.props.use_decimal);
+		
 		return (
 			<tr key={outcome.outcomeId}>
 				<td className="text-left">{outcome.name}</td>
